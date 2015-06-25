bbed72cf1c598370b9c30f17f6dc75a91fc6fc4e
AsherB <obeastoo@live.com>
2015-06-25 10:08:05 -0700

    added for loops in the Game class

--- src/com/asher/game/Game.java
+++ src/com/asher/game/Game.java
@@ -1,30 +1,45 @@
 package com.asher.game;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Shape;
 
+import com.asher.game.entities.BaseEntity;
+
 public class Game extends BasicGame 
 {
-
-	public Game (String gamename) 
-	{
+	private ArrayList<BaseEntity> entityList;
+	
+	
+	public Game (String gamename) {
 		super(gamename);
+		this.entityList = new ArrayList<BaseEntity>();
+		
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {}
 	
 	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+	public void update(GameContainer gc, int i) throws SlickException {
+		for(BaseEntity e : entityList ) {
+			e.update(gc, i);
+		}
+	}
 
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		g.drawString("Howdy!", 100, 100);
-	
+		for(int i = 0; i < entityList.size(); i++) {
+			entityList.get(i).render(container, g);
+		}
+		
 	}
+	
+	
 	
 }
