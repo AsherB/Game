dfe25274cb5f5f8898f76dfde90a544d61db2b3a
AsherB <obeastoo@live.com>
2015-06-26 12:03:38 -0700

    .

---
+++ src/com/asher/funGame/entities/Player.java
@@ -0,0 +1,28 @@
+package com.asher.funGame.entities;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Player extends BaseEntity {
+
+	public Player(int zIndex) {
+		super(zIndex);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		
+
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		
+
+	}
+
+}
