ad3ae8010f4c37dcfdb9fee4895da2fe7dd3b3b7
AsherB <obeastoo@live.com>
2015-07-02 10:56:09 -0700

    .

--- src/com/asher/funGame/Main.java
+++ src/com/asher/funGame/Main.java
@@ -1,11 +1,15 @@
 package com.asher.funGame;
 
+import java.io.File;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.setProperty("java.library.path", "libs");
+		System.setProperty("org.lwjgl.librarypath", new File("libs/natives").getAbsolutePath());
 		try {
 			AppGameContainer app = new AppGameContainer(new Game("Doge the Bullets"));
 			app.setDisplayMode(1920, 1080, true);
